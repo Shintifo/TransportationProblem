4469481bd69bcb7cdec9eeb753e10be3ef90a6c8
t <timofey.brayko@gmail.com>
2023-11-08 16:31:02 +0000

    Done NW, russel Draft for Vogel

--- main.py
+++ main.py
@@ -1,58 +1,73 @@
-from enum import Enum
-
 import numpy as np
 
 
-class TaskType(Enum):
-	MAXIMIZE = 0,
-	MINIMIZE = 1
-
-
-def north_west(s: np.array(int), d: np.array(int), c: np.array(int)):
+def north_west(supply, demand, cost):
+	s = np.copy(supply)
+	d = np.copy(demand)
+	c = np.copy(cost)
 	current_state = (0, 0)
 	distribution = np.zeros(shape=c.shape)
-	ans = 0
+	total_cost = 0
 	while current_state[0] < s.shape[0] and current_state[1] < d.shape[0]:
-		distribution[current_state] = min(s[current_state[0]], d[current_state[1]])
-		if distribution[current_state] == s[current_state[0]]:
-			next_state = tuple(map(sum, zip(current_state, (1, 0))))
-		else:
-			next_state = tuple(map(sum, zip(current_state, (0, 1))))
-		s[current_state[0]] -= distribution[current_state]
-		d[current_state[1]] -= distribution[current_state]
-		ans += distribution[current_state] * c[current_state]
+		x, y = current_state
+		distribution[current_state] = min(s[x], d[y])
+
+		next_move = (1, 0) if distribution[current_state] == s[x] else (0, 1)
+		next_state = tuple(map(sum, zip(current_state, next_move)))
+
+		s[x] -= distribution[current_state]
+		d[y] -= distribution[current_state]
+		total_cost += distribution[current_state] * c[current_state]
 		current_state = next_state
-	print("Distribution:\n", distribution)
-	print("Total cost: ", ans)
+	print("Total cost: ", total_cost)
 
 
-def russel(s: np.array(int), d: np.array(int), c: np.array(int)):
-	max_rows = np.asarray([max(elem) for elem in c])
-	max_columns = np.asarray([max(c[0:, column]) for column in range(d.shape[0])])
+def russel(supply, demand, cost):
+	s = np.copy(supply)
+	d = np.copy(demand)
+	c = np.copy(cost)
+	total_cost = 0
+	distribution = np.zeros(shape=c.shape)
 
-	delta = np.zeros(shape=c.shape)
-	for i in range(s.shape[0]):
-		for j in range(d.shape[0]):
-			delta[i][j] = +c[i][j] - max_columns[j] - max_rows[i]
+	min_rows = c.max(axis=1)
+	min_columns = c.max(axis=0)
+	delta = c - min_columns - min_rows[:, np.newaxis]
 
-	print(delta)
-	# while True:
-	max_val = np.unravel_index(a.argmax(), a.shape)
+	while np.any(s != 0) and np.any(d != 0):
+		min_val = np.unravel_index(delta.argmin(), delta.shape)
+		delta[min_val] = 0
+		if s[min_val[0]] == d[min_val[1]] == 0:
+			continue
+		distribution[min_val] = min(s[min_val[0]], d[min_val[1]])
+		s[min_val[0]] -= distribution[min_val]
+		d[min_val[1]] -= distribution[min_val]
+		total_cost += distribution[min_val] * c[min_val]
+	print("Total cost:", total_cost)
 
 
-def vogel(S: np.array(int), D: np.array(int), C: np.array(int)):
-	print("vogel")
+def vogel(supply, demand, cost):
+	s = np.copy(supply)
+	d = np.copy(demand)
+	c = np.copy(cost)
+	min_column_values = np.min(c, axis=0)
+	second_column_min_values = np.partition(c, 1, axis=0)[1]
+
+	min_row_values = np.min(c, axis=1)
+	second_min_row_values = np.partition(c, 1, axis=1)[:, 1]
+
+
+
+
 
 
 if __name__ == '__main__':
-	tass_type = TaskType.MAXIMIZE
-	accuracy = 0.001
-	s = np.array([160, 140, 170])
-	d = np.array([120, 50, 190, 110])
+	# TODO: define M
+	s = np.array([300, 400, 500])
+	d = np.array([250, 350, 400, 200])
+	c = np.array([[3, 1, 7, 4],
+				  [2, 6, 5, 9],
+				  [8, 3, 3, 2]])
 
-	c = np.array([[7, 8, 1, 2],
-				  [4, 5, 9, 8],
-				  [9, 2, 3, 6]])
 
-	a = np.array([[1, 1, 21], [20, 20, 20]])
-# russel(s, d, c)
+	# north_west(s, d, c)
+	# russel(s, d, c)
